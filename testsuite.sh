791a015de76a716324b4bc2d69a7e245510af9c8
Joachim Breitner <joachim.breitner@itomig.de>
2009-06-12 09:59:39 +0000

    add a testsuite to wine-app-packager

---
+++ testsuite.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+echo <<''
+wine-app-packager test suite. This will create a package from 7z and check if
+everything is ok.
+
+if [ -d ~/.wine-7z-master ]
+then
+	echo "~/.wine-7z-master already exists, can not run testsuite"
+	exit 1
+fi
+
+set -e
+set -x
+
+sourcedir="$(dirname "$PWD/$0")"
+tmpdir=$(mktemp -t -d wine-app-packager-test.XXXXXXXX)
+trap "rm -rf \"$tmpdir\"" EXIT
+cd $tmpdir
+
+WAP="$sourcedir/wine-app-packager"
+
+if [ ! -x $WAP ]
+then
+	echo "$WAP is not executable"
+	exit 1
+fi
+
+$WAP init 7z <<''
+7z
+1.2.3
+
+
+if [ ! -d 7z-1.2.3 ]
+then
+	echo "init did not create the 7z-1.2.3 directory"
+	exit 1
+fi
+
+cd 7z-1.2.3
+
+$WAP prepare
+
+if [ ! -d ~/.wine-7z-master ]
+then
+	echo "init did not create the ~/.wine-7z-master directory"
+	exit 1
+fi
+
+$WAP run $sourcedir/testdata/7z465.exe /S
+
+$WAP commit
+
+echo This should fail:
+! dpkg-buildpackage -uc -us 
+
+echo Setting .exe name in script
+sed -i -e 's/EXE=.*/EXE='\''c:\\Programme\\7-Zip\\7zFM.exe'\''/' 7z
+
+echo And now this should work:
+dpkg-buildpackage -uc -us 
+
+if ! debc | grep -q ./opt/wineapps/wine-7z/drive_c/Programme/7-Zip/7zFM.exe
+then
+	echo "debc does not contain ./opt/wineapps/wine-7z/drive_c/Programme/7-Zip/7zFM.exe"
+	debc
+	exit 1
+fi
+
+echo "Test suite successfully finished"
+
